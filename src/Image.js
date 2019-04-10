155abed3e01c028a2001c9c2fd3559c85004daf6
Sharif <sharif4box@gmail.com>
2019-04-10 14:33:59 +0200

    Updated

--- src/Image.js
+++ src/Image.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./Image.css"
+export default class Image extends React.Component {
 
-export default function Image() {
-  return (
-    <div>
-      Image
-    </div>
-  )
+  constructor(props) {
+    super(props)
+    this.state = {
+      url: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://source.unsplash.com/collection/190727/1600x900')
+    .then((response)=> { this.setState({ url: response.url })})
+
+  }
+
+  render() {
+    return (
+      
+        <img src={this.state.url}></img>
+        
+    )
+  }
 }
+
